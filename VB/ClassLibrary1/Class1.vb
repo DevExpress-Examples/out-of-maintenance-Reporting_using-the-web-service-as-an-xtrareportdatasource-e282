656fde609b0ad221a88fc5e44fba3af7e65b7aa4
DevExpressExampleBot <fakeEmail>
2020-12-31 17:52:37 +0300

    autogenerate VB

---
+++ VB/ClassLibrary1/Class1.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace ClassLibrary1
+	Public Class Class1
+	End Class
+
+End Namespace
---
+++ VB/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+
+Namespace WindowsApplication1
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub chartControl1_ObjectHotTracked(ByVal sender As Object, ByVal e As DevExpress.XtraCharts.HotTrackEventArgs) Handles chartControl1.ObjectHotTracked
+
+		End Sub
+		Private selectedSeriesPoint As SeriesPoint
+		Private Sub chartControl1_ObjectSelected(ByVal sender As Object, ByVal e As DevExpress.XtraCharts.HotTrackEventArgs) Handles chartControl1.ObjectSelected
+			If e.HitInfo.SeriesPoint IsNot Nothing Then
+				If selectedSeriesPoint <> e.HitInfo.SeriesPoint Then
+					selectedSeriesPoint = e.HitInfo.SeriesPoint
+					Me.chartControl1.Refresh()
+				End If
+			End If
+		End Sub
+		Private Sub chartControl1_CustomDrawSeriesPoint(ByVal sender As Object, ByVal e As DevExpress.XtraCharts.CustomDrawSeriesPointEventArgs) Handles chartControl1.CustomDrawSeriesPoint
+			If e.SeriesPoint = selectedSeriesPoint Then
+				e.SeriesDrawOptions.Color = Color.Red
+			End If
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			If Not Me.button1.IsHandleCreated Then Return
+
+			selectedSeriesPoint = Nothing
+		End Sub
+	End Class
+End Namespace
