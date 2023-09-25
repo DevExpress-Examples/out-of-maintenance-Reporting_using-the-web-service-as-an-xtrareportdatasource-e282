99d8951cf5e4ad1318b637477ab6a676ca6bd581
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-26 00:21:33 +0400

    Change example status to out of maintenance

--- CS/ClassLibrary1/Class1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ClassLibrary1 {
-    public class Class1 {
-    }
-
-}
--- VB/ClassLibrary1/Class1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Text
-
-Namespace ClassLibrary1
-	Public Class Class1
-	End Class
-
-End Namespace
